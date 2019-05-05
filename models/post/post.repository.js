28652c5e028c62533ad2ca164382b9c61a1c6610
ball <ball@balls-MacBook-Pro.local>
2019-05-05 16:24:08 +0700

    create post model

---
+++ models/post/post.repository.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose'
+
+const postRepository = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+    },
+    avatar: {
+        type: String,
+    },
+    likes: [{
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user'
+        }
+    }],
+    comment: [{
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        text: {
+            type: String,
+            required: true,
+        },
+        avatar: {
+            type: String,
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+
+export default mongoose.model('post', postRepository)
